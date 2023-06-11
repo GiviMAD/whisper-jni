1add62487764df41a6ce869daf6500aa7299cedb
Miguel Álvarez <miguelwork92@gmail.com>
2023-06-11 22:01:49 +0200

    add macos arm64

--- build_macos.sh
+++ build_macos.sh
@@ -1,12 +1,36 @@
 set -xe
 
-cc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin -O3 -DNDEBUG -std=c11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 -c ./src/main/native/whisper/ggml.c -o ./src/main/native/ggml.o
+AARCH=${1:-$(uname -m)}
+case "$AARCH" in
+  x86_64|amd64)
+    AARCH=x86_64
+    AARCH_NAME=amd64
+    TARGET_VERSION=11.0
+    CFLAGS="$CFLAGS -mf16c -mfma -mavx -mavx2"
+    CXXFLAGS=""
+    ;;
+  arm64|aarch64)
+    AARCH=arm64
+    AARCH_NAME=arm64
+    TARGET_VERSION=11.0
+    CFLAGS=""
+	CXXFLAGS=""
+    ;;
+  *)
+    echo Unsupported arch $AARCH
+    ;;
+    
+esac
+INCLUDE_JAVA="-I $JAVA_HOME/include"
+TARGET=$AARCH-apple-macosx$TARGET_VERSION
 
-g++ -c -std=c++11 -O3 -DNDEBUG -fPIC -pthread -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin \
+cc -O3 --target="$TARGET" -std=c11 -arch "$AARCH" \
+$CFLAGS -DNDEBUG -fPIC -pthread -c ./src/main/native/whisper/ggml.c -o ./src/main/native/ggml.o
+
+g++ -c -std=c++11 -arch "$AARCH" -O3 -DNDEBUG -fPIC -pthread $INCLUDE_JAVA \
+--target="$TARGET" \
 src/main/native/io_github_givimad_whisperjni_WhisperJNI.cpp -o src/main/native/io_github_givimad_whisperjni_WhisperJNI.o
 
-g++ -dynamiclib -I src/main/native -o src/main/resources/macos-amd64/libwhisperjni.dylib src/main/native/ggml.o src/main/native/io_github_givimad_whisperjni_WhisperJNI.o -lc
+g++ -arch "$AARCH" --target="$TARGET" -dynamiclib -Wl,-headerpad_max_install_names -I src/main/native -o src/main/resources/macos-$AARCH_NAME/libwhisperjni.dylib src/main/native/ggml.o src/main/native/io_github_givimad_whisperjni_WhisperJNI.o -lc
 
 rm -rf src/main/native/*.o
-
-
