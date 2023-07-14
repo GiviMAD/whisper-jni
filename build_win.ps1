7f2a2532fc21cc6b4887977c14e7fafd9d655f08
Miguel Álvarez <miguelwork92@gmail.com>
2023-07-14 14:48:04 +0200

    link win64 lib statically

--- build_win.ps1
+++ build_win.ps1
@@ -4,7 +4,7 @@ gcc -O3 -DNDEBUG -std=c11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 -c .\src\main
 
 g++ -c -std=c++11 -O3 -DNDEBUG -fPIC -pthread -I $env:JAVA_HOME\include -I $env:JAVA_HOME\include\win32 src\main\native\io_github_givimad_whisperjni_WhisperJNI.cpp -o src\main\native\io_github_givimad_whisperjni_WhisperJNI.o
 
-g++ -shared -I src\main\native -o src\main\resources\win-amd64\libwhisperjni.dll src\main\native\ggml.o src\main\native\io_github_givimad_whisperjni_WhisperJNI.o
+g++ -shared -static -I src\main\native -o src\main\resources\win-amd64\libwhisperjni.dll src\main\native\ggml.o src\main\native\io_github_givimad_whisperjni_WhisperJNI.o
 
 if ($LastExitCode -ne 0) {
     Write-Error "Unable to build library"
