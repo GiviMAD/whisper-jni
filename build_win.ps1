0c7aa875b5049892db1431ced89ca696a44931c9
Miguel Álvarez <miguelwork92@gmail.com>
2023-11-09 19:31:27 +0100

    fix windows build

--- build_win.ps1
+++ build_win.ps1
@@ -1,16 +1,13 @@
 Set-PSDebug -Trace 1
-$env:CLIENT_KEY="xxxxxxxxxxxxxxxxx";
-$env:CFLAGS="-O3 -DNDEBUG -std=c11 -fPIC -pthread -D_XOPEN_SOURCE=600 -mf16c -mfma -mavx -mavx2";
-$env:CXXFLAGS="-std=c++11 -O3 -DNDEBUG -fPIC -pthread -mf16c -mfma -mavx -mavx2";
 # build ggml objects
-gcc $env:CFLAGS -c .\src\main\native\whisper\ggml.c -o .\src\main\native\ggml.o
-gcc $env:CFLAGS -c .\src\main\native\whisper\ggml-backend.c -o .\src\main\native\ggml-backend.o
-gcc $env:CFLAGS -c .\src\main\native\whisper\ggml-quants.c -o .\src\main\native\ggml-quants.o
-gcc $env:CFLAGS -c .\src\main\native\whisper\ggml-alloc.c -o .\src\main\native\ggml-alloc.o
+gcc -c -DNDEBUG -O3 -std=c11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 .\src\main\native\whisper\ggml.c -o .\src\main\native\ggml.o
+gcc -c -DNDEBUG -O3 -std=c11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 .\src\main\native\whisper\ggml-backend.c -o .\src\main\native\ggml-backend.o
+gcc -c -DNDEBUG -O3 -std=c11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 .\src\main\native\whisper\ggml-quants.c -o .\src\main\native\ggml-quants.o
+gcc -c -DNDEBUG -O3 -std=c11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 .\src\main\native\whisper\ggml-alloc.c -o .\src\main\native\ggml-alloc.o
 # build whisper object
-g++ -c $env:CXXFLAGS -I src\main\native\whisper -c .\src\main\native\whisper\whisper.cpp -o .\src\main\native\whisper.o
+g++ -c -DNDEBUG -O3 -std=c++11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 -I src\main\native\whisper -c .\src\main\native\whisper\whisper.cpp -o .\src\main\native\whisper.o
 # build whisper jni wrapper object
-g++ -c $env:CXXFLAGS -I $env:JAVA_HOME\include -I $env:JAVA_HOME\include\win32 -I src\main\native\whisper src\main\native\io_github_givimad_whisperjni_WhisperJNI.cpp -o src\main\native\io_github_givimad_whisperjni_WhisperJNI.o
+g++ -c -DNDEBUG -O3 -std=c++11 -fPIC -pthread -mf16c -mfma -mavx -mavx2 -I $env:JAVA_HOME\include -I $env:JAVA_HOME\include\win32 -I src\main\native\whisper src\main\native\io_github_givimad_whisperjni_WhisperJNI.cpp -o src\main\native\io_github_givimad_whisperjni_WhisperJNI.o
 # build tmp whisper dll to link non full jni wrapper version agains it
 g++ -shared -static -I src\main\native -I src\main\native\whisper -o whisper.dll src\main\native\whisper.o src\main\native\ggml.o src\main\native\ggml-alloc.o src\main\native\ggml-quants.o src\main\native\ggml-backend.o 
 # link full whisper jni shared object
